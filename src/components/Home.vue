38c1a833f702344a82ee77396aa288dede9e935f
Иванов Николай Алексеевич <na_ivanov@betboom.org>
2021-08-20 16:50:22 +0300

    continue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="home">
     <h1 class="home__title">Home</h1>
-    <!-- <el-input type="text" placeholder="What we will watch?" v-model="taskTitle" @keyup.enter="newTask" clearable></el-input>
-    <el-input
-      class="textarea"
-      type="textarea"
-      :rows="2"
-      v-model="taskDescription"
-      @keyup.enter="newTask"
-      clearable>
-    </el-input> -->
     <div class="home__wrap">
       <input class="home-input" type="text" placeholder="What we will watch?" v-model="taskTitle" @keyup.enter="newTask">
       <textarea class="home-textarea" type="textarea" v-model="taskDescription" @keyup.enter="newTask"></textarea>
+      <div class="home__radio-container">
+        <div class="home__radio">
+          <input class="home__radio-input" type="radio" id="radioFilm" value="Film" v-model="whatWatch">
+          <label class="home__radio-label" for="radioFilm">Film</label>
+        </div>
+        <div class="home__radio">
+          <input class="home__radio-input" type="radio" id="radioSerial" value="Serial" v-model="whatWatch">
+          <label class="home__radio-label" for="radioSerial">Serial</label>
+        </div>
+      </div>
     </div>
     <el-card v-for="film in films" :key="film.name" class="box-card">
       <div class="box-card__container">
         <div class="box-card__title">
-          <el-tag
-            v-for="item in itemFilm"
-            :key="item.name"
-            :type="itemFilm.type">
-            {{ item.name }}
+          <el-tag>
+            {{ film.type }}
           </el-tag>
           <div class="box-card__text">Total time:</div>
         </div>
@@ -43,19 +41,19 @@ export default {
       taskTitle: '',
       taskDescription: '',
       taskId: 3,
-      itemFilm: [
-        { name: 'Film', type: '' }
-      ],
+      whatWatch: 'Film',
       films: [
         {
           id: '1',
           name: 'IT',
-          text: 'fsdagkjdsgbfvukydvgkvkyuzbhkjdjfbsuygdfauigbidfasdfvbshjdjvdjhsjkbfsdfdgsdfuygsfhbvckljdiauswq'
+          text: 'fsdagkjdsgbfvukydvgkvkyuzbhkjdjfbsuygdfauigbidfasdfvbshjdjvdjhsjkbfsdfdgsdfuygsfhbvckljdiauswq',
+          type: 'Film'
         },
         {
           id: '2',
           name: 'lakjsbdf',
-          text: 'fsdagkjdsgbfvukydv'
+          text: 'fsdagkjdsgbfvukydv',
+          type: 'Serial'
         }
       ]
     }
@@ -68,7 +66,8 @@ export default {
       this.films.push({
         id: this.taskId,
         name: this.taskTitle,
-        text: this.taskDescription
+        text: this.taskDescription,
+        type: this.whatWatch
       })
       this.taskId += 1
       this.taskTitle = ''
@@ -90,6 +89,46 @@ export default {
       width: 100%;
       display: flex;
       flex-direction: column;
+    }
+    &__radio {
+      display: inline-block;
+      margin-top: 20px;
+      &:last-child {
+        margin-left: 20px;
+      }
+      &-container {
+        display: flex;
+        align-items: center;
+      }
+      &-label {
+        // margin-left: 10px;
+        display: inline-block;
+        cursor: pointer;
+        padding: 0px 15px;
+        line-height: 34px;
+        border: 1px solid #409EFF;
+        border-radius: 6px;
+        user-select: none;
+        font-size: 14px;
+        font-family: "Helvetica Neue", Helvetica;
+        color: #409EFF;transition: 0.3s all;
+        // &:hover {
+        //   color: #666;
+        // }
+      }
+      &-input {
+        transition: 0.3s all;
+        display: none;
+        &:checked + .home__radio-label {
+          background:#ecf5ff;
+          border: 1px solid #ecf5ff;
+          transition: 0.3s all;
+        }
+        &:disabled + .home__radio-label {
+          background: #efefef;
+          color: #666;
+        }
+      }
     }
     &-input {
       padding: 10px;
